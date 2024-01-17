b0838bcbf9d9231790d614dcf5e410e6a537bd63
Bijit Pal Chaudhuri <Bijitpalchaudhuri@gmail.com>
2024-01-17 21:51:33 +0530

    prep for ajax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
-
+import { AdminModule } from './admin/admin.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    AdminModule,
     AppRoutingModule
   ],
   providers: [],
