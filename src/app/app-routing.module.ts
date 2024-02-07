7a7977edff2fc6c7bd11f9f1e207cca4f0823dda
Bijit Pal Chaudhuri <Bijitpalchaudhuri@gmail.com>
2024-02-08 03:35:24 +0530

    JWT setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { AboutComponent } from './admin/about/about.component';
 import { ProjectsComponent } from './admin/projects/projects.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"dashboard", component:DashboardComponent},
   {path:"about",component:AboutComponent},
   {path:"projects",component:ProjectsComponent},
-  {path:"", redirectTo:"dashboard",pathMatch:"full"}
+  {path:"login", component:LoginComponent},
+  {path:"", redirectTo:"login",pathMatch:"full"}
 ];
 
 @NgModule({
